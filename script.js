fd31e5ff5ce46d3db4e812e957be77a3fdc75a61
RedeemedWanderer <170952724+RedeemedWanderer@users.noreply.github.com>
2024-06-03 11:48:28 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code for interactivity can be added here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    // Add any JavaScript functionality needed for the site
+});
